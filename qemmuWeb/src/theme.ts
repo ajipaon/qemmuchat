2db978a2b0e41cfc76ebce58b1b406a30dccf230
ajipaon <ajisetiawan883@gmail.com>
2025-05-08 15:29:47 +0700

    sasad

--- qemmuWeb/src/theme.ts
+++ qemmuWeb/src/theme.ts
@@ -1,44 +1,98 @@
-import {createTheme, rem} from "@mantine/core";
+import { createTheme, rem } from "@mantine/core";
 
 export const theme = createTheme({
-    colors: {
-        // Add your color
-        deepBlue: [
-            '#eef3ff',
-            '#dce4f5',
-            '#b9c7e2',
-            '#94a8d0',
-            '#748dc1',
-            '#5f7cb8',
-            '#5474b4',
-            '#44639f',
-            '#39588f',
-            '#2d4b81',
-        ],
-        // or replace default theme color
-        blue: [
-            '#eef3ff',
-            '#dee2f2',
-            '#bdc2de',
-            '#98a0ca',
-            '#7a84ba',
-            '#6672b0',
-            '#5c68ac',
-            '#4c5897',
-            '#424e88',
-            '#364379',
-        ],
-    },
+  colors: {
+    // Neobrutalism bold colors
+    primary: [
+      "#FFE766",
+      "#FFE04D",
+      "#FFD833",
+      "#FFD11A",
+      "#FFCB00",
+      "#E6B700",
+      "#CCA300",
+      "#B28F00",
+      "#997B00",
+      "#806700",
+    ],
+    secondary: [
+      "#FF6B6B",
+      "#FF5252",
+      "#FF3838",
+      "#FF1F1F",
+      "#FF0505",
+      "#E60000",
+      "#CC0000",
+      "#B20000",
+      "#990000",
+      "#800000",
+    ],
+  },
 
-    shadows: {
-        md: '1px 1px 3px rgba(0, 0, 0, .25)',
-        xl: '5px 5px 3px rgba(0, 0, 0, .25)',
-    },
+  shadows: {
+    xs: "4px 4px 0 rgba(0, 0, 0, .95)",
+    sm: "5px 5px 0 rgba(0, 0, 0, .95)",
+    md: "6px 6px 0 rgba(0, 0, 0, .95)",
+    lg: "8px 8px 0 rgba(0, 0, 0, .95)",
+    xl: "10px 10px 0 rgba(0, 0, 0, .95)",
+  },
 
-    headings: {
-        fontFamily: 'Roboto, sans-serif',
-        sizes: {
-            h1: { fontSize: rem(36) },
+  headings: {
+    fontFamily: "Space Grotesk, sans-serif",
+    sizes: {
+      h1: { fontSize: rem(36) },
+      h2: { fontSize: rem(30) },
+      h3: { fontSize: rem(24) },
+      h4: { fontSize: rem(20) },
+      h5: { fontSize: rem(16) },
+      h6: { fontSize: rem(14) },
+    },
+  },
+
+  defaultRadius: "0",
+
+  components: {
+    Button: {
+      styles: (theme) => ({
+        root: {
+          border: "3px solid black",
+          borderRadius: "0",
+          background: theme.colors.primary[4],
+          color: "black",
+          fontWeight: 700,
+          "&:hover": {
+            transform: "translate(-2px, -2px)",
+            boxShadow: "4px 4px 0 rgba(0, 0, 0, .95)",
+          },
         },
+      }),
     },
+    Card: {
+      styles: {
+        root: {
+          border: "3px solid black",
+          borderRadius: "0",
+        },
+      },
+    },
+    Input: {
+      styles: {
+        input: {
+          border: "3px solid black",
+          borderRadius: "0",
+          "&:focus": {
+            border: "3px solid black",
+          },
+        },
+      },
+    },
+    Paper: {
+      styles: {
+        root: {
+          border: "3px solid black",
+          borderRadius: "0",
+        },
+      },
+    },
+  },
 });
